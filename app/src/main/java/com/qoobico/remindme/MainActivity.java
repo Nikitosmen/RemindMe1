1174dba7b6c67edf14ab4759694d93c3eaf0b390
Nike <xxxmonster.gamerxxx@yandex.ru>
2016-08-15 15:00:09 +0500

    Create MainActivity class and layout

---
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.qoobico.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import alpha.mess.remindme.R;
+
+/**
+ * Created by Nike on 15.08.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_main);
+    }
+}
