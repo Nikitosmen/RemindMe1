83ab4434e6bf5435c00e4ecefd950538d68a6900
Nike <xxxmonster.gamerxxx@yandex.ru>
2016-08-16 19:25:39 +0500

    Added navigation button on NavView

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,6 +1,8 @@
 package com.qoobico.remindme;
 
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toolbar;
@@ -8,14 +10,19 @@ import alpha.mess.remindme.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.acticity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView ();
     }
 
     private void initToolbar() {
@@ -29,5 +36,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
